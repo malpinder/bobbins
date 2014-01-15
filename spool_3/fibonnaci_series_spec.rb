19458018d040e6e93979378fdfa977a8dd97f80f
Mal Pinder <mal.pinder@fundingcircle.com>
2014-01-15 16:03:05 +0000

    Spool 3. Complicated logic, working with classes.

---
+++ spool_3/fibonnaci_series_spec.rb
@@ -0,0 +1,42 @@
+require './spec_helper'
+require 'prime'
+
+def generate_fibonnaci(number)
+  series = [0, 1]
+  __
+end
+
+describe "generate_fibonnaci" do
+  it "returns the first 2 fibonnaci numbers when asked for 2 numbers" do
+    generate_fibonnaci(2).should eq [0, 1]
+  end
+
+  it "returns the first 5 fibonnaci numbers when asked for 5 numbers" do
+    generate_fibonnaci(5).should eq [0, 1, 1, 2, 3]
+  end
+
+  it "returns the first 10 fibonnaci numbers when asked for 10 numbers" do
+    generate_fibonnaci(10).should eq [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+  end
+end
+
+def prime_fibonnaci(number)
+  series = generate_fibonnaci(number)
+  __
+end
+
+describe "prime_fibonnaci" do
+  it "returns no primes from the first 2 fibonacci numbers" do
+    # Remember, 0 and 1 aren't prime!
+    prime_fibonnaci(2).should eq []
+  end
+  it "returns the primes from the first 10 fibonacci numbers" do
+    prime_fibonnaci(10).should eq [2, 3, 5, 13]
+  end
+  it "returns the primes from the first 20 fibonacci numbers" do
+    prime_fibonnaci(20).should eq [2, 3, 5, 13, 89, 233, 1597]
+  end
+end
+
+# What happens when you ask for even larger sets? Why?
+# Can you think of a way around it?
---
+++ spool_3/kitten_spec.rb
@@ -0,0 +1,76 @@
+require './spec_helper'
+
+class Kitten
+  __
+end
+
+describe Kitten do
+
+  let(:kitten) { Kitten.new }
+
+  it "purrs when stroked" do
+    kitten.stroke.should eq "purr"
+  end
+
+  it "hisses when scolded" do
+    kitten.scold.should eq "hsssss!"
+  end
+
+  describe "feed" do
+    it "returns true when fed ketchup on fish" do
+      kitten.feed('ketchup on fish').should be_true
+    end
+
+    it "returns true when fed chicken ragu" do
+      kitten.feed('chicken ragu').should be_true
+    end
+
+    it "returns true when fed a milk jug" do
+      kitten.feed('milk jug').should be_true
+    end
+
+    it "returns false when fed a milk bowl" do
+      kitten.feed('milk bowl').should be_true
+    end
+
+    it "returns false when fed fish and chips" do
+      kitten.feed('fish and chips').should be_false
+    end
+
+    it "returns false when fed fish" do
+      kitten.feed('fish').should be_false
+    end
+
+    it "returns false when fed ketchup" do
+      kitten.feed('ketchup').should be_false
+    end
+
+    # Bonus points if you work out a pattern and build a general solution.
+  end
+
+  describe "is_hungry?" do
+    it "is hungry at first" do
+      kitten.should be_hungry
+    end
+
+    it "is not hungry after eating" do
+      kitten.feed('chicken ragu')
+      kitten.should_not be_hungry
+    end
+
+    it "is still hungry after refusing to eat" do
+      kitten.feed('fish and chips')
+      kitten.should be_hungry
+    end
+
+    it "is hungry again three seconds after eating" do
+      kitten.feed('chicken ragu')
+      sleep 3
+      kitten.should be_hungry
+    end
+
+    # What happens to this test if we give the kitten a better metabolism
+    # and it takes longer to get hungry?
+    # Can you think of a way around that?
+  end
+end
