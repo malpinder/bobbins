390a7a76bebfad2e64df2fdfc19aa071f5b97f0f
Alex Sunderland <alex.sunderland@gmail.com>
2015-05-28 10:15:02 +0100

    Update RSpec syntax for spool 3 specs

--- spool_3/fibonnaci_series_spec.rb
+++ spool_3/fibonnaci_series_spec.rb
@@ -8,15 +8,15 @@ end
 
 describe "generate_fibonnaci" do
   it "returns the first 2 fibonnaci numbers when asked for 2 numbers" do
-    generate_fibonnaci(2).should eq [0, 1]
+    expect(generate_fibonnaci(2)).to eq [0, 1]
   end
 
   it "returns the first 5 fibonnaci numbers when asked for 5 numbers" do
-    generate_fibonnaci(5).should eq [0, 1, 1, 2, 3]
+    expect(generate_fibonnaci(5)).to eq [0, 1, 1, 2, 3]
   end
 
   it "returns the first 10 fibonnaci numbers when asked for 10 numbers" do
-    generate_fibonnaci(10).should eq [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    expect(generate_fibonnaci(10)).to eq [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
   end
 end
 
@@ -28,13 +28,13 @@ end
 describe "prime_fibonnaci" do
   it "returns no primes from the first 2 fibonacci numbers" do
     # Remember, 0 and 1 aren't prime!
-    prime_fibonnaci(2).should eq []
+    expect(prime_fibonnaci(2)).to eq []
   end
   it "returns the primes from the first 10 fibonacci numbers" do
-    prime_fibonnaci(10).should eq [2, 3, 5, 13]
+    expect(prime_fibonnaci(10)).to eq [2, 3, 5, 13]
   end
   it "returns the primes from the first 20 fibonacci numbers" do
-    prime_fibonnaci(20).should eq [2, 3, 5, 13, 89, 233, 1597]
+    expect(prime_fibonnaci(20)).to eq [2, 3, 5, 13, 89, 233, 1597]
   end
 end
 
--- spool_3/kitten_spec.rb
+++ spool_3/kitten_spec.rb
@@ -9,40 +9,40 @@ describe Kitten do
   let(:kitten) { Kitten.new }
 
   it "purrs when stroked" do
-    kitten.stroke.should eq "purr"
+    expect(kitten.stroke).to eq "purr"
   end
 
   it "hisses when scolded" do
-    kitten.scold.should eq "hsssss!"
+    expect(kitten.scold).to eq "hsssss!"
   end
 
   describe "feed" do
     it "returns true when fed ketchup on fish" do
-      kitten.feed('ketchup on fish').should be_true
+      expect(kitten.feed('ketchup on fish')).to be_true
     end
 
     it "returns true when fed chicken ragu" do
-      kitten.feed('chicken ragu').should be_true
+      expect(kitten.feed('chicken ragu')).to be_true
     end
 
     it "returns true when fed a milk jug" do
-      kitten.feed('milk jug').should be_true
+      expect(kitten.feed('milk jug')).to be_true
     end
 
     it "returns false when fed a milk bowl" do
-      kitten.feed('milk bowl').should be_false
+      expect(kitten.feed('milk bowl')).to be_false
     end
 
     it "returns false when fed fish and chips" do
-      kitten.feed('fish and chips').should be_false
+      expect(kitten.feed('fish and chips')).to be_false
     end
 
     it "returns false when fed fish" do
-      kitten.feed('fish').should be_false
+      expect(kitten.feed('fish')).to be_false
     end
 
     it "returns false when fed ketchup" do
-      kitten.feed('ketchup').should be_false
+      expect(kitten.feed('ketchup')).to be_false
     end
 
     # Bonus points if you work out a pattern and build a general solution.
@@ -50,7 +50,7 @@ describe Kitten do
 
   describe "is_hungry?" do
     it "is hungry at first" do
-      kitten.should be_hungry
+      expect(kitten).to be_hungry
     end
 
     it "is not hungry after eating" do
@@ -60,13 +60,13 @@ describe Kitten do
 
     it "is still hungry after refusing to eat" do
       kitten.feed('fish and chips')
-      kitten.should be_hungry
+      expect(kitten).to be_hungry
     end
 
     it "is hungry again three seconds after eating" do
       kitten.feed('chicken ragu')
       sleep 3
-      kitten.should be_hungry
+      expect(kitten).to be_hungry
     end
 
     # What happens to this test if we give the kitten a better metabolism
